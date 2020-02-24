8f53338cc62d8d21e3e0dd1cd39b6224b61cecd1
Quan Trinh <trinhquan1123@yahoo.com>
2020-02-24 11:02:29 +0800

    add some routes for users

--- app/main/controllers/users_controller.py
+++ app/main/controllers/users_controller.py
@@ -3,6 +3,7 @@ from flask_restplus import Resource
 
 from app.main.utils.dto import UserDTO
 from app.main.services.user_service import UserService
+from app.main.services.visit_service import VisitService
 
 api = UserDTO.api
 _user = UserDTO.user
@@ -24,3 +25,32 @@ class Users(Resource):
 		""" Create a new user """
 		data = request.json
 		return service.create_user(data)
+
+@api.route('/<public_id>')
+@api.param('public_id', 'User\'s identifier')
+@api.response(404, 'User not found')
+class User(Resource):
+	@api.doc('Get an user given his identifier')
+	@api.marshal_with(_user)
+	def get(self, public_id):
+		""" Get an user given his identifier """
+		user = service.find_user(public_id)
+		if not user:
+			api.abort(404, 'User with id {public_id} cannot be found'.format(public_id=public_id))
+		else:
+			return user
+
+@api.route('/<public_id>/enter')
+@api.param('public_id', 'User\'s identifier')
+@api.response(404, 'User not found')
+@api.response(201, 'User entered store')
+class UserEnters(Resource):
+	@api.doc('Processes that happen when an user enters a store')
+	def post(self):
+		""" Automatically create a new visit and cart when an user enters a store """
+		user = service.find_user(public_id)
+		if not user:
+			api.abort(404, 'User with id {public_id} cannot be found'.format(public_id=public_id))
+
+		visit = visit_service.create_visit({ 'user_id': user.id })
+
--- seed.py
+++ seed.py
@@ -4,13 +4,24 @@ from random import randint
 from manage import app
 from app.main import db
 from app.main.services.product_service import ProductService
-# from app.main.services.user_service import UserService
+from app.main.services.user_service import UserService
 from app.main.models.user import User
 from app.main.models.visit import Visit
 from app.main.models.cart import Cart
 from app.main.models.product import Product
 
 app.app_context().push()
+
+# user names
+users = (
+	"Alexane Collins",
+	"Giovani Romaguera",
+	"Jailyn Pagac",
+	"Sigmund Legros",
+	"Ken Wehner",
+	"Josh Ryan",
+	"Zackary Bauch"
+)
 
 # List of product names for seeding
 products = (
@@ -33,9 +44,17 @@ for table in reversed(db.metadata.sorted_tables):
 
 db.session.commit()
 
+product_service = ProductService()
+user_service = UserService()
+
+# Create users from names
+for name in users:
+	email = '_'.join(name.lower().split(' '))
+	user = { 'email': "{email}@email.com".format(email=email) }
+	user_service.create_user(user)
+
 # Add random products
 for name in products:
-	service = ProductService()
-	data = { 'name': name, 'price': randint(300, 1000) }
+	product = { 'name': name, 'price': randint(300, 1000) }
 	for _ in range(randint(1, 20)):
-		service.create_product(data)
+		product_service.create_product(product)
